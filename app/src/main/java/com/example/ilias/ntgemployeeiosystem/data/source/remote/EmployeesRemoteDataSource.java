bbd5f2ff86cbb8a8fd3e9e3e1a56d1ee75e4022d
Ahmed Mesawer <AhmedMesawer@github.com>
2018-02-22 14:24:21 +0200

    ui change

--- app/src/main/java/com/example/ilias/ntgemployeeiosystem/data/source/remote/EmployeesRemoteDataSource.java
+++ app/src/main/java/com/example/ilias/ntgemployeeiosystem/data/source/remote/EmployeesRemoteDataSource.java
@@ -1,5 +1,7 @@
 package com.example.ilias.ntgemployeeiosystem.data.source.remote;
 
+import com.example.ilias.ntgemployeeiosystem.data.Employee;
+import com.example.ilias.ntgemployeeiosystem.data.WorkDay;
 import com.example.ilias.ntgemployeeiosystem.data.source.EmployeesDataSource;
 
 /**
@@ -7,4 +9,23 @@ import com.example.ilias.ntgemployeeiosystem.data.source.EmployeesDataSource;
  */
 
 public class EmployeesRemoteDataSource implements EmployeesDataSource {
+    @Override
+    public void getEmployee(String mac) {
+
+    }
+
+    @Override
+    public void addEmployee(Employee employee) {
+
+    }
+
+    @Override
+    public void addWorkDay(String mac, WorkDay workDay) {
+
+    }
+
+    @Override
+    public void setEmployeeOut(String mac, String date, WorkDay workDay) {
+
+    }
 }
--- app/src/main/java/com/example/ilias/ntgemployeeiosystem/in_out/IOPresenter.java
+++ app/src/main/java/com/example/ilias/ntgemployeeiosystem/in_out/IOPresenter.java
@@ -1,8 +1,28 @@
 package com.example.ilias.ntgemployeeiosystem.in_out;
 
+import com.example.ilias.ntgemployeeiosystem.data.source.EmployeesDataSource;
+
 /**
  * Created by ilias on 21/02/2018.
  */
 
 public class IOPresenter implements IOContract.Presenter {
+    private IOContract.View view;
+    private EmployeesDataSource employeesDataSource;
+
+    public IOPresenter(IOContract.View view, EmployeesDataSource employeesDataSource) {
+        this.view = view;
+        this.employeesDataSource = employeesDataSource;
+        view.setPresenter(this);
+    }
+
+    @Override
+    public void setEmployeeAttended() {
+
+    }
+
+    @Override
+    public void setEmployeeWentOut() {
+
+    }
 }
