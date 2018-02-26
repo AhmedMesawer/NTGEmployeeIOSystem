9d1f5494790d275db808fddd608f4ea08f78e3a0
MagdyEl-Sayed <magdyelsayed47@gmail.com>
2018-02-26 14:22:57 +0200

    ?!

--- app/src/main/java/com/example/ilias/ntgemployeeiosystem/in_out/MainActivity.java
+++ app/src/main/java/com/example/ilias/ntgemployeeiosystem/in_out/MainActivity.java
@@ -1,20 +1,39 @@
 package com.example.ilias.ntgemployeeiosystem.in_out;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.ilias.ntgemployeeiosystem.R;
 
+import java.util.UUID;
+
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.bind(this);
+        String uniqueID = UUID.randomUUID().toString();
 
+        Toast.makeText(this, getMacId(), Toast.LENGTH_SHORT).show();
+    }
+    public String getMacId() {
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        return wifiInfo.getBSSID();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_employee, menu);
